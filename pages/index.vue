8acc1e588dfa8c9bef1e61b11a00143fa523b89a
André Nolasco <nolasco3d@gmail.com>
2022-11-07 22:37:31 -0300

    Try gh-pages lib

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="(item, index) in words" :key="index">
+      <NuxtLink :to="`/word/${item}`">{{item}}</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const words = ref([
+    'hello',
+    'apple',
+    'table'
+  ])
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/word/[slug].vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="true">
+    <p>word:{{$route.params.slug}}</p>
+
+    <!-- <pre v-if="data">{{data}}</pre> -->
+  </div>
+  <div v-else>
+    Loading...
+  </div>
+</template>
+
+<script setup lang="ts">
+  const route = useRoute();
+  
+  // const {data, pending} = await useFetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${route.params.slug}`);
+
+  // console.log(data)
+</script>
+
+<style scoped>
+
+</style>
