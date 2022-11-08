936d12609b20355eb321c7c55d538ae81fbce064
André Nolasco <nolasco3d@gmail.com>
2022-11-08 13:51:01 -0300

    adjusts on configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/gh-pages-capiroto/'
+    // baseURL: '/gh-pages-capiroto/'
   },
   modules:['@nuxtjs/tailwindcss'],
   tailwindcss: {
-    viewer: false
-  }
+    viewer: false,
+    cssPath: '~/assets/css/tailwind.css'
+  },
+  css: ['~/assets/css/tailwind.css']
 })
